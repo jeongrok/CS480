48e04c9381b9f39538684935d69eb4e7a4d72298
jeongrok <jyu248@emory.edu>
2023-04-23 16:10:31 -0400

    Topic Modelling added

--- googlescholarrank.py
+++ googlescholarrank.py
@@ -13,7 +13,12 @@ now = datetime.datetime.now()
 ENDYEAR = now.year
 MAX_CSV_FNAME = 255
 
-# Websession Parameters
+
+#Dealing with Captcha
+ROBOT_KW=['unusual traffic from your computer network', 'not a robot']
+raw_input = input
+
+#Web Parameters
 GSCHOLAR_URL = 'https://scholar.google.com/scholar?start={}&q=allintitle%3A{}&hl=en&as_sdt=0,5'
 STARTYEAR_URL = '&as_ylo={}'
 ENDYEAR_URL = '&as_yhi={}'
@@ -71,6 +76,53 @@ def get_author(content):
             break
     return out
 
+def setup_driver():
+    try:
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
+        from selenium.common.exceptions import StaleElementReferenceException
+    except Exception as e:
+        print(e)
+        print("Please install Selenium and chrome webdriver for manual checking of captchas")
+
+    print('Loading...')
+    chrome_options = Options()
+    chrome_options.add_argument("disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("start-maximized")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    return driver
+
+def get_element(driver, xpath, attempts=5, _count=0):
+    '''Safe get_element method with multiple attempts'''
+    try:
+        element = driver.find_element_by_xpath(xpath)
+        return element
+    except Exception as e:
+        if _count<attempts:
+            sleep(1)
+            get_element(driver, xpath, attempts=attempts, _count=_count+1)
+        else:
+            print("Element not found")
+
+def get_content_with_selenium(url):
+    if 'driver' not in globals():
+        global driver
+        driver = setup_driver()
+    driver.get(url)
+
+    # Get element from page
+    el = get_element(driver, "/html/body")
+    c = el.get_attribute('innerHTML')
+
+    if any(kw in el.text for kw in ROBOT_KW):
+        raw_input("Solve captcha manually and press enter here to continue...")
+        el = get_element(driver, "/html/body")
+        c = el.get_attribute('innerHTML')
+
+
+    return c.encode('utf-8')
+
 
 def main():
     keyword, number_of_results, start_year, end_year = get_command_line_args()
@@ -97,6 +149,15 @@ def main():
         url = GSCHOLAR_MAIN_URL.format(str(n), keyword.replace(' ', '+'))
         page = session.get(url)
         c = page.content
+
+        # Using Selenium if Robot checking is detected
+        if any(kw in c.decode('ISO-8859-1') for kw in ROBOT_KW):
+            print("Robot checking detected, handling with selenium (if installed)")
+            try:
+                c = get_content_with_selenium(url)
+            except Exception as e:
+                print("No success. The following error was raised:")
+                print(e)
         # Create parser
         soup = BeautifulSoup(c, 'html.parser', from_encoding='utf-8')
         mydivs = soup.findAll("div", {"class": "gs_or"})
